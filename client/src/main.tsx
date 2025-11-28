5a6eb4e84ca38a0155ffac4d63402b5ef9e1a678
sedrickwall <sedrick.wall.jr@gmail.com>
2025-11-28 20:32:52 +0000

    Testmode update

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { initTestMode } from "@/lib/testMode";
 
-
+// Initialize test mode BEFORE rendering the app
+initTestMode();
 
 createRoot(document.getElementById("root")!).render(<App />);
